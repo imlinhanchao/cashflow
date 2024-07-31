873360ab38b179486fb8120cb1e0ed883fc0f9ff
Hancel.Lin <imlinhanchao@gmail.com>
2024-07-31 21:57:05 +0800

    ✨ feat: 定义统计数据结构

--- src/core/Dto/common.dto.ts
+++ src/core/Dto/common.dto.ts
@@ -9,3 +9,30 @@ export interface QueryReqDto {
   username?: string;
   [key: string]: any;
 }
+
+export interface StatisticDto {
+  source: Record<string, DataSourceDto>;
+  options: any;
+}
+
+export interface DataSourceDto {
+  field: DataFieldDto[];
+  where: any;
+}
+
+export interface DataFieldDto {
+  field: string;
+  label: string;
+  fun: SQLFn[];
+}
+
+export interface SQLFn {
+  name: string;
+  params: SQLFnParam[];
+  operator: string;
+}
+
+export interface SQLFnParam {
+  type: 'string' | 'col' | 'fn';
+  value: string | SQLFn;
+}
